35fdac2d9bc85b2dba003e91a671c6d17d7c13b1
Pete Warden <pete@usefulsensors.com>
2025-06-13 17:57:02 -0700

    Added websocket support to express script

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.send('Matchmaking server, websocket access only');
+  res.end();
+});
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Matchmaker app listening on port ${port}`);
 });
